67114cb60282f481e373c86e550b59b6714546a3
isisrg <72613800+isisrg@users.noreply.github.com>
2022-09-20 01:30:13 +0200

    Add files via upload
    Lap record implemented, not showing best/worst lap.

--- app.js
+++ app.js
@@ -5,7 +5,13 @@ const $lapResetButton = document.getElementById("lapResetButton");
 let timerId;
 
 let isRunning = false;
-let isFirstLap = true;
+let hasStarted = true;
+
+const $lapTable = document.getElementById("lapTable");
+let lapTableRow;
+let lapNumber = 1;
+let lapMinutes = (lapSeconds = lapMilliseconds = lapHundredths = 0);
+let currentLap = (startingTimeCurrentLap = 0);
 
 // ----------------------- //
 function formatAndPrintTimeValues() {
@@ -13,6 +19,11 @@ function formatAndPrintTimeValues() {
   seconds = seconds.toString().padStart(2, "0");
   hundredths = hundredths.toString().padStart(2, "0");
   $timer.innerText = `${minutes}:${seconds}.${hundredths}`;
+}
+function formatAndPrintLapValues() {
+  lapMinutes = lapMinutes.toString().padStart(2, "0");
+  lapSeconds = lapSeconds.toString().padStart(2, "0");
+  lapHundredths = lapHundredths.toString().padStart(2, "0");
 }
 
 function updateTimeValues() {
@@ -23,15 +34,31 @@ function updateTimeValues() {
   formatAndPrintTimeValues();
 }
 
+function updateCurrentLapValues() {
+  lapMilliseconds += 10;
+  lapMinutes = Math.floor(lapMilliseconds / 60000);
+  lapSeconds = Math.floor(lapMilliseconds / 1000) % 60;
+  lapHundredths = Math.floor((lapMilliseconds % 1000) / 10);
+  formatAndPrintLapValues();
+}
+
+function registerNewLap() {
+  currentLap = `${lapMinutes}:${lapSeconds}.${lapHundredths}`;
+  lapTableRow = $lapTable.insertRow(0);
+  lapTableRow.innerText = `Lap ${lapNumber} ${currentLap}`;
+  lapTableRow.id = `lap-${lapNumber}`;
+}
+
 // ----------------------- //
 $startStopButton.onclick = () => {
   if (!isRunning) {
-    if (isFirstLap) {
-      isFirstLap = false;
+    if (hasStarted) {
+      hasStarted = false;
     } else {
       $lapResetButton.innerText = "Lap";
     }
     $startStopButton.innerText = "Stop";
+    startingTimeCurrentLap = milliseconds;
     timerId = setInterval(updateTimeValues, 10);
   } else {
     $startStopButton.innerText = "Start";
@@ -42,10 +69,20 @@ $startStopButton.onclick = () => {
 };
 
 $lapResetButton.onclick = () => {
-  if (!isRunning && !isFirstLap) {
-    $startStopButton.innerText = "Start";
-    $lapResetButton.innerText = "Lap";
-    minutes = seconds = milliseconds = hundredths = 0;
-    formatAndPrintTimeValues();
+  if (!hasStarted) {
+    if (!isRunning) {
+      $startStopButton.innerText = "Start";
+      $lapResetButton.innerText = "Lap";
+      minutes = seconds = milliseconds = hundredths = 0;
+      formatAndPrintTimeValues();
+      $lapTable.innerHTML = "";
+      lapNumber = 1;
+    } else if (isRunning) {
+      lapMilliseconds = milliseconds - startingTimeCurrentLap;
+      updateCurrentLapValues();
+      registerNewLap();
+      ++lapNumber;
+      startingTimeCurrentLap = milliseconds;
+    }
   }
 };
